0fb30de5c454c4e481f4e3af5315f9224996e85a
AbinavDWH <ans.offical.net@gmail.com>
2024-12-16 19:35:58 +0530

    modified:   determine.c

--- determine.c
+++ determine.c
@@ -9,6 +9,10 @@ int main(){
     int det=0;
     for(int i=0;i<3;i++){
         det+=(arr[0][(i+2)%3]*(arr[1][i]*arr[2][(i+1)%3]-arr[1][(i+1)%3]*arr[2][i]));
+/* Under this is given gpt bhai for good understanding logical and readable
+for (int i = 0; i < 3; i++) {
+        det += arr[0][i] * (arr[1][(i+1)%3] * arr[2][(i+2)%3] - arr[1][(i+2)%3] * arr[2][(i+1)%3]);
+    }*/
     }
     printf("%d",det);
 }
