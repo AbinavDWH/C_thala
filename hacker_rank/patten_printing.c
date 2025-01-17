7dedebb6a8806fdc5dd35366c9631e27076f3a43
AbinavDWH <ans.offical.net@gmail.com>
2025-01-17 13:24:15 +0530

    new file:   hacker_rank/Querying the Document.c 	modified:   hacker_rank/patten_printing.c 	new file:   project_messeger/patten_printing 	new file:   project_messeger/patten_printing.c

--- hacker_rank/patten_printing.c
+++ hacker_rank/patten_printing.c
@@ -1,27 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+
+    
+    
+  #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
-{
+void printPattern(int n) {
+    int size = 2 * n - 1;  // Size of the pattern (size x size)
+    int pattern[size][size];
 
-    int n,tmp=0;
-    scanf("%d", &n);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            // if(i<j){
-            //     tmp=n-i;
-            // }
-            // else tmp=n;
-           if(i==n-1||i==0 || j==0||j==n-1) printf("%d ",tmp);
-            else printf("  ");
-    }printf("\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int min = i < j ? i : j;  // Minimum of row or column index
+            min = min < size - i ? min : size - i - 1;  // Minimum of (size - row index)
+            min = min < size - j ? min : size - j - 1;  // Minimum of (size - column index)
+            pattern[i][j] = n - min;
+        }
     }
-    
-    
-  
+
+    // Print the pattern
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", pattern[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n ;
+      // Change this value to generate the pattern for different n
+    scanf("%d",&n);
+    printPattern(n);
+    return 0;
+}
+
   	
 
     
-}
