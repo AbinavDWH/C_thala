0a85f76f403a390564930f78ae5b5411b4276571
AbinavDWH <ans.offical.net@gmail.com>
2025-03-22 20:22:57 +0530

    new file:   linked_7.c

---
+++ linked_7.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    int ex;
+    struct node* next;
+}*pol1=NULL,*pol2=NULL,*result=NULL;
+
+typedef struct node NODE;
+
+NODE *ins(NODE *p,int x,int ex){
+    NODE *new_node=(NODE *)malloc(sizeof(NODE));
+    new_node->data=x;
+    new_node->ex=ex;
+    new_node->next=p;
+    p=new_node;
+    return p;
+}
+
+void in(int x,int ex){
+    NODE *temp=(NODE *)malloc(sizeof(NODE));
+    temp->data=x;
+    temp->ex=ex;
+    temp->next=NULL;
+    if(result==NULL){
+        result=temp;
+    }
+    else{
+        NODE *last=result;
+        while(last->next!=NULL){
+            last=last->next;
+            }
+            last->next=temp;
+            }
+}
+
+void pol_add(){
+    NODE *p1=pol1,*p2=pol2;
+    while(p1!=NULL || p2!=NULL){
+        while (p1!=NULL &&p2!=NULL)
+        {
+            /* code */
+        
+        
+        if(p1->ex==p2->ex){
+            in(p1->data+p2->data,p1->ex);
+            p1=p1->next;
+            p2=p2->next;
+        }
+        else if(p1->ex>p2->ex){
+            in(p1->data,p1->ex);
+            p1=p1->next;
+        }
+        else{
+            in(p2->data,p2->ex);
+            p2=p2->next;
+            }
+        }
+        if(pol1!=NULL){
+            while(p1!=NULL){
+                in(p1->data,p1->ex);
+                p1=p1->next;
+                }
+        }
+        if(pol2!=NULL){
+            while(p2!=NULL){
+                in(p2->data,p2->ex);
+                p2=p2->next;
+                }
+                }
+        
+    }
+    
+
+}
+
+void display(NODE *head){
+    NODE *p=head;
+    while(p!=NULL){
+        printf("%d",p->data);
+        if(p->ex!=0){
+            printf("x^%d",p->ex);
+            }
+            if(p->next!=NULL){
+                printf("+");
+                }
+                p=p->next;
+                }
+                printf("\n");
+                
+}
+int main(){
+    int x,ex;
+    while (1)
+    {
+        /* code */
+        scanf("%d %d",&x,&ex);
+        if(x==0 && ex==0){
+            break;
+            }
+            pol1=ins(pol1,x,ex);
+    }
+    while (1)
+    {
+        /* code */
+        scanf("%d %d",&x,&ex);
+        if(x==0 && ex==0){
+            break;
+            }
+            pol2=ins(pol2,x,ex);
+            
+    }
+    display(pol1);
+    display(pol2);
+    printf("Sum of polynomials:\n");
+    pol_add();
+    display(result);
+}
