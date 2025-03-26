9c0cd073dd7da4047440ab9e554a76b0b871ae2b
AbinavDWH <ans.offical.net@gmail.com>
2025-03-26 19:18:44 +0530

    modified:   linked_10.c

--- linked_10.c
+++ linked_10.c
@@ -9,7 +9,7 @@ struct node {
 
 typedef struct node NODE;
 
-void insend(NODE **head,x){
+void insend(NODE **head,int x){
     NODE *nn=(NODE *)malloc(sizeof(NODE));
     nn->data=x;
     nn->next=NULL;nn->pre=NULL;
@@ -26,11 +26,11 @@ void insend(NODE **head,x){
     }
 }
 
-void insbeg(NODE **head,x){
+void insbeg(NODE **head,int x){
     NODE *nn=(NODE *)malloc(sizeof(NODE));
     nn->data=x;
     nn->next=*head;nn->pre=NULL;
-    *head->pre=nn;
+    (*head)->pre=nn;
     *head=nn;
 }
 
@@ -41,13 +41,14 @@ void dis(NODE *temp){
     }
 }
 
-void insbegpos(NODE **head,n,x){
+void insbegpos(NODE **head,int n,int x){
     NODE *temp=*head;
-    int flag=0
+    int flag=0;
     while(temp!=NULL){
         if(temp->data==n){
-        flag=1
+        flag=1;
         break;}
+        temp=temp->next;
     }
     if(flag==0){
     printf("Value not found in the list");
@@ -58,16 +59,21 @@ void insbegpos(NODE **head,n,x){
     nn->data=x;
     nn->next=temp;
     nn->pre=temp->pre;
-    temp->pre=nn;
+    if (temp->pre != NULL)
+    temp->pre->next = nn;
+else
+    *head = nn;  // Update head if inserting at the beginning
+temp->pre = nn;
 }
 
-void insendpos(NODE **head,n,x){
+void insendpos(NODE **head,int n,int x){
     NODE *temp=*head;
-    int flag=0
+    int flag=0;
     while(temp!=NULL){
         if(temp->data==n){
-        flag=1
+        flag=1;
         break;}
+        temp=temp->next;
     }
     if(flag==0){
     printf("Value not found in the list");
@@ -84,11 +90,11 @@ void insendpos(NODE **head,n,x){
 }
 
 void delbeg(NODE **head){
-    NODE *temp=*head;
-    *head=*head->next;
-    *head->pre=NULL;
-    printf("The linked list after deletetion from the beginning is:")
-    free(temp);
+    NODE *temp=*head,*summa=*head;
+    temp=temp->next;
+    temp->pre=NULL;
+    printf("The linked list after deletetion from the beginning is:");
+    free(summa);
     
 }
 
@@ -103,7 +109,7 @@ void delend(NODE **head){
     free(tmp);
 }
 
-void delbegpos(NODE **head,n,x){
+void delbegpos(NODE **head,int n){
     NODE *temp=*head;
     int flag=0;
     while(temp!=NULL){
@@ -111,31 +117,58 @@ void delbegpos(NODE **head,n,x){
             flag=1;
             break;
         }
+        temp=temp->next;
     }
     if(flag==0){
     printf("Value not found in the list");
     return ;
         
     }
-    NODE *tmp=temp->pre->pre;
+    NODE *tmp=temp->pre;
     temp->pre->pre->next=temp;
     temp->pre=temp->pre->pre;
-    
+    free(tmp);
 }
+
+void delafpos(NODE **head,int x){
+    NODE *temp=*head;int f=0;
+    while(temp!=NULL && temp->next!=NULL){
+        if(temp->data==x){
+            f=1;
+            break;
+        }
+        temp=temp->next;
+    }
+    if(f==0){
+        printf("%d\n",temp->data);
+        return ;
+    }
+    NODE *tmp=temp->next;
+    temp->next=temp->next->next;
+    temp->next->next->pre=temp;
+    free(tmp);
+}
+
+
 
 int main(){
     int n,input,flag=0,modi;
     NODE *list=NULL;
     do{
-        scanf("%d",input);
+        scanf("%d",&input);
+        if(input==11){
+break;
+        }
         switch(input){
             case 1:
             while(1){
-                scanf("%d",n);
+                scanf("%d",&n);
                 if(n!=-1){
-                insend(&list,n)
-                    break;
+                insend(&list,n);
+                    
                 }
+                else
+                break;
             }
             flag=1;
             printf("LINKED LIST CREATED\n");
@@ -151,39 +184,49 @@ int main(){
             case 3:
             if(flag==1){
                 scanf("%d",&n);
-                insbeg(&head,n);
+                insbeg(&list,n);
                 printf("The linked list after insertion at the beginning is:\n");
-                
+                dis(list);
             }
             break;
             case 4:
             if(flag==1){
                 scanf("%d",&n);
-                insend(&head,n);
+                insend(&list,n);
                 printf("The linked list after insertion at the end is:\n");
+                dis(list);
             }
             break;
             case 5:
-                scanf("%d %d",modi,n);
-                insbegpos(&head,modi,n);
-                dis(head);
+                scanf("%d %d",&modi,&n);
+                insbegpos(&list,modi,n);
+                dis(list);
                 break;
             case 6:
-                scanf("%d %d",modi,n);
-                insbegpos(&head,modi,n);
-                dis(head);
+                scanf("%d %d",&modi,&n);
+                insendpos(&list,modi,n);
+                dis(list);
                 break;
             case 7:
-                delbeg(&head);
-                dis(head);
+                delbeg(&list);
+                dis(list);
                 break;
             case 8:
-                delend(&head);
-                dis(head);
+                delend(&list);
+                dis(list);
                 break;
             case 9:
-            
-            
+                scanf("%d",&n);
+                delbegpos(&list,n);
+                dis(list);
+                break;
+            case 10:
+                    scanf("%d",&n);
+                    delafpos(&list,n);
+                    dis(list);
+                    break;
+            default:
+            printf("lll");
             
         }
     }while(input!=11);
